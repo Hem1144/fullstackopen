142e38500db377f6177047de41872268a9058371
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-07-20 16:29:39 +0545

    feat: completed course info upto 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,30 +1,32 @@
 function App() {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Heading course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
 
-      <Total parts={parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }
 
-const Heading = (props) => {
+const Header = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
@@ -32,8 +34,8 @@ const Heading = (props) => {
   );
 };
 const Content = (props) => {
-  return props.parts.map((prop, index) => (
-    <Part part={prop.name} exercises={prop.exercises} key={index} />
+  return props.parts.map((props, index) => (
+    <Part part={props.name} exercises={props.exercises} key={index} />
   ));
 };
 const Part = (props) => {
@@ -44,15 +46,12 @@ const Part = (props) => {
   );
 };
 
-
-
 const Total = (props) => {
-  let totalExe = 0;
-  props.parts.forEach((ele) => {
-    totalExe += ele.exercises;
+  let totalExercise = 0;
+  props.parts.map((ele) => {
+    return (totalExercise += ele.exercises);
   });
-  return <p>Number of exercises {totalExe}</p>;
+  return <p>Number of exercises {totalExercise}</p>;
 };
-
 
 export default App;
