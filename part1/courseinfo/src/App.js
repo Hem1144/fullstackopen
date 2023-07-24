2cb66a3f5423d20ffeba162b7ff214ee4a898ace
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-07-24 20:36:28 +0545

    feat: completed exercise of part 2-a

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -50,9 +50,7 @@ const App = () => {
   return (
     <div>
       <h1>Web Development curriculum</h1>
-      {courses.map((course) => (
-        <Course key={course.id} course={course} />
-      ))}
+      <Course course={courses} />
     </div>
   );
 };
--- part1/courseinfo/src/components/Course.js
+++ part1/courseinfo/src/components/Course.js
@@ -1,16 +1,59 @@
-import React from "react";
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
-
 const Course = ({ course }) => {
   return (
     <div>
-      
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={course} />
+      {course.map((ele) => {
+        return (
+          <div key={ele.id}>
+            <Header text={ele.name} />
+            <Content content={ele.parts} />
+          </div>
+        );
+      })}
     </div>
+  );
+};
+
+const Header = ({ text }) => {
+  return <h2>{text}</h2>;
+};
+
+const Total = ({ parts }) => {
+  return (
+    <p>
+      <b>
+        <span>total of </span>
+        {parts.reduce(
+          (accum, currtVal) => (accum = accum + currtVal.exercises),
+          0
+        )}
+        <span> excercises</span>
+      </b>
+    </p>
+  );
+};
+
+const Part = ({ courseName, courseExcercise }) => {
+  return (
+    <p>
+      {courseName} {courseExcercise}
+    </p>
+  );
+};
+
+const Content = ({ content }) => {
+  return (
+    <>
+      {content.map((val) => {
+        return (
+          <Part
+            key={val.id}
+            courseName={val.name}
+            courseExcercise={val.exercises}
+          />
+        );
+      })}
+      <Total parts={content} />
+    </>
   );
 };
 
