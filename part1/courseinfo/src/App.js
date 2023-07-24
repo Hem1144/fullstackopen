52820ac9fa1e7317ff6c2243f7c0d86686c1fd9c
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-07-24 15:23:23 +0545

    feat: completed course info 2.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,57 +1,30 @@
-function App() {
+import React from "react";
+import Course from "./components/Course";
+
+const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-}
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
+  return <Course course={course} />;
 };
 
-const Content = (props) => {
-  return props.parts.map((props, index) => (
-    <Part part={props.name} exercises={props.exercises} key={index} />
-  ));
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  );
-};
-
-const Total = (props) => {
-  let totalExercise = 0;
-  props.parts.map((ele) => {
-    return (totalExercise += ele.exercises);
-  });
-  return <p>Number of exercises {totalExercise}</p>;
-};
 export default App;
