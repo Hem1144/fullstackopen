c86f868a960627ca05491da37fc8ff29a8e9d89e
Hemlal Dulal <dulalhemlal986@gmail.com>
2023-07-26 16:48:15 +0545

    feat: completed phonebook upto 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ searchTerm, handleSearchChange }) => {
   return (
@@ -54,6 +55,13 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response);
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
