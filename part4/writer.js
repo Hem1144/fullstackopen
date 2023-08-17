d7ac99f15d9422270cf484852311d46e8c51fe30
Hem1144 <hemlaldulal.986@gmail.com>
2023-08-17 10:51:13 +0545

    feat: Re-arranging the Schema

--- part4/writer.js
+++ part4/writer.js
@@ -17,6 +17,15 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 });
 
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+  
+});
+
 const Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/api/blogs", (request, response) => {
