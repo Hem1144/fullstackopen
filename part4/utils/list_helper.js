8eac73ab4ff8678a468008ae79383e4c8936da4a
Hem1144 <hemlaldulal.986@gmail.com>
2023-08-17 16:29:21 +0545

    feat: Completed 4.4, helper functions and unit tests, step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0);
+};
+const zeroLike = (blogs) => {
+  return 0
+};
+
 module.exports = {
   dummy,
+  totalLikes,
+  zeroLike
 };
