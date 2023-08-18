b2849781853d885bbb35f26dc98a6fbb2959f37a
Hem1144 <hemlaldulal.986@gmail.com>
2023-08-18 13:29:40 +0545

    Completed upto Helper function of unit tests 4.6 of part4

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -40,3 +40,54 @@ describe("total likes", () => {
     expect(result).toBe(5);
   });
 });
+
+describe("favorite blog", () => {
+  const blogs = [
+    {
+      title: "Blog 1",
+      author: "Author 1",
+      likes: 10,
+    },
+    {
+      title: "Blog 2",
+      author: "Author 2",
+      likes: 15,
+    },
+    {
+      title: "Blog 3",
+      author: "Author 3",
+      likes: 12,
+    },
+  ];
+
+  test("finds the blog with the most likes", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    expect(result).toEqual({
+      title: "Blog 2",
+      author: "Author 2",
+      likes: 15,
+    });
+  });
+});
+
+describe("most blogs", () => {
+  test("returns the author with most blogs", () => {
+    const blogs = [
+      { author: "Author A" },
+      { author: "Author B" },
+      { author: "Author A" },
+      { author: "Author C" },
+    ];
+    const result = listHelper.mostBlogs(blogs);
+    expect(result).toEqual({
+      author: "Author A",
+      blogs: 2,
+    });
+  });
+
+  test("returns null for empty list", () => {
+    const blogs = [];
+    const result = listHelper.mostBlogs(blogs);
+    expect(result).toBe(null);
+  });
+});
