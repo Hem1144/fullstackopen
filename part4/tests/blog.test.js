0cb2c700a97da4d5145882c934359063c2b77e30
Hem1144 <hemlaldulal.986@gmail.com>
2023-08-25 12:46:54 +0545

    feat: Completed upto 4.10

--- part4/tests/blog.test.js
+++ part4/tests/blog.test.js
@@ -37,23 +37,41 @@ test("GET /api/blogs returns correct amount of blog posts in JSON format", async
 
 test("there are two notes", async () => {
   const response = await api.get("/api/blogs");
-
   expect(response.body).toHaveLength(2);
 });
 
-test("the first note is about HTTP methods", async () => {
+test("the first note is about HTML", async () => {
   const response = await api.get("/api/blogs");
-
   expect(response.body[0].title).toBe("HTML is easy");
 });
 
 test("blogs have property 'id' instead of '_id'", async () => {
   const response = await api.get("/api/blogs");
-
   response.body.forEach((blog) => {
     expect(blog.id).toBeDefined();
     expect(blog._id).toBeUndefined();
   });
+});
+
+test("a new blog can be added", async () => {
+  const newBlog = {
+    title: "My Title",
+    author: "My Author",
+    url: "myUrl",
+    likes: 8,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(initialBlogs.length + 1);
+
+  const titles = response.body.map((blog) => blog.title);
+  expect(titles).toContain(newBlog.title);
 });
 
 afterAll(async () => {
