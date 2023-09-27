333f8e23fc8c7e15e9bac76c064472ae49e00313
Hem1144 <hemlaldulal.986@gmail.com>
2023-09-27 13:31:04 +0545

    feat: Completed 6.11 by using redux-toolkit

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,29 +1,3 @@
-// const filterReducer = (state = "", action) => {
-//   switch (action.type) {
-//     case "SET_FILTER":
-//       return action.filter;
-//     case "CLEAR_FILTER":
-//       return "";
-//     default:
-//       return state;
-//   }
-// };
-
-// export const setFilter = (filter) => {
-//   return {
-//     type: "SET_FILTER",
-//     filter,
-//   };
-// };
-
-// export const clearFilter = () => {
-//   return {
-//     type: "CLEAR_FILTER",
-//   };
-// };
-
-// export default filterReducer;
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const filterSlice = createSlice({
