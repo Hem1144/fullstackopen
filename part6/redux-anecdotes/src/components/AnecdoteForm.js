1af3b9ba251a00383cf8a8de66467041e0efbaf8
Hem1144 <hemlaldulal.986@gmail.com>
2023-09-28 14:49:54 +0545

    feat: Completed adding new anecdote in backend

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,18 +1,30 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import anecdoteService from "../services/anecdotes";
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
 
-    dispatch(createAnecdote({ content, id: getId(), votes: 0 }));
+    const newAnecdote = {
+      content,
+      id: getId(),
+      votes: 0,
+    };
+
+    try {
+      const createdAnecdote = await anecdoteService.createAnecdote(newAnecdote);
+      dispatch(createAnecdote(createdAnecdote));
+    } catch (error) {
+      console.error("An error occurred while creating the anecdote:", error);
+    }
   };
 
   return (
