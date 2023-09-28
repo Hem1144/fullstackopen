e6e8e4d3d11e93973636ae68544ffdc940fd5615
Hem1144 <hemlaldulal.986@gmail.com>
2023-09-28 11:57:47 +0545

    feat: did 6.14, When the application launches, fetch the anecdotes from the backend implemented using json-server

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,20 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import anecdoteService from "./services/anecdotes";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
   const notification = useSelector((state) => state.notification);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService.getAll().then((response) => {
+      dispatch(initializeAnecdotes(response));
+    });
+  }, [dispatch]);
 
   return (
     <>
