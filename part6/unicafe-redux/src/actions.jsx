b64aec8beb4ad85a4cc4809af95532dca4787c21
Hemlal Dulal <hemlaldulal.986@gmail.com>
2023-11-03 09:20:15 +0545

    feat: Adding part6 files

---
+++ part6/unicafe-redux/src/actions.jsx
@@ -0,0 +1,15 @@
+export const incrementGood = () => ({
+  type: 'GOOD',
+});
+
+export const incrementOk = () => ({
+  type: 'OK',
+});
+
+export const incrementBad = () => ({
+  type: 'BAD',
+});
+
+export const resetFeedback = () => ({
+  type: 'ZERO',
+});
